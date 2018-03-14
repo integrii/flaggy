15f69ece59528470d97fee064813ec3276f78ec4
Eric Greer <EricGreer@Gmail.com>
2018-03-14 15:06:29 +0800

    drop NewPositionalValue constructor

--- positionalValue.go
+++ positionalValue.go
@@ -1,10 +1,5 @@
 package flaggy
 
-import (
-	"fmt"
-	"os"
-)
-
 // PositionalValue represents a value which is determined by its position
 // relative to where a subcommand was detected.
 type PositionalValue struct {
@@ -12,13 +7,4 @@ type PositionalValue struct {
 	Description   string
 	AssignmentVar *string // the var that will get this variable
 	Position      int     // the position, not including switches, of this variable
-}
-
-// NewPositionalValue creates a new positional value at a depth relative to
-// the last positional argument or subcommand
-func NewPositionalValue(relativeDepth int) {
-	if relativeDepth < 1 {
-		fmt.Println("Flaggy: Position of positional arguments must never be below 1")
-		os.Exit(2)
-	}
 }
