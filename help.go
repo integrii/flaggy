79ff4e2db60539bac3dc3b5a14c8e969e5b62efd
Eric Greer <EricGreer@Gmail.com>
2018-03-23 12:08:39 +0800

    improved help spacing

--- help.go
+++ help.go
@@ -1,20 +1,20 @@
 package flaggy
 
 // defaultHelpTemplate is the help template used by default
-const defaultHelpTemplate = `{{.CommandName}}{{if .Description}} - {{.Description}}{{end}}
-{{if .PrependMessage}}{{.PrependMessage}}
-{{end}}{{if .Positionals}}{{end}}
+const defaultHelpTemplate = `{{.CommandName}}{{if .Description}} - {{.Description}}{{end}}{{if .PrependMessage}}{{.PrependMessage}}{{end}}{{if .Positionals}}
+
   Positional Variables:
-{{range .Positionals}}    {{.Name}} (Position {{.Position}}){{if .Required}} (Required){{end}}{{if .Description}} {{.Description}}{{end}}
-{{end}}
-{{ if .Subcommands}}  Subcommands:{{end}}
-{{range .Subcommands}}    {{.LongName}}{{if .ShortName}} ({{.ShortName}}){{end}}{{if .Position}} (Position {{.Position}}){{end}}{{if .Description}} {{.Description}}{{end}}
-{{end}}
-{{if (gt (len .StringFlags) 0) | (gt (len .IntFlags) 0) | (gt (len .BoolFlags) 0)}} Flags:
-{{range .StringFlags}}    --{{.LongName}}{{if .ShortName}} (-{{.ShortName}}){{end}}{{if .Description}} {{.Description}}{{end}}{{end}}
-{{range .IntFlags}}    --{{.LongName}}{{if .ShortName}} (-{{.ShortName}}){{end}}{{if .Description}} {{.Description}}{{end}}{{end}}
-{{range .BoolFlags}}    --{{.LongName}}{{if .ShortName}} (-{{.ShortName}}){{end}}{{if .Description}} {{.Description}}{{end}}{{end}}{{end}}
-{{if (gt (len .AppendMessage) 0) | (gt (len .Message) 0)}}
-{{end}}{{if .AppendMessage}}{{.AppendMessage}}{{end}}{{if .Message}}
-{{.Message}}
+{{range .Positionals}}
+    {{.Name}} (Position {{.Position}}){{if .Required}} (Required){{end}}{{if .Description}} {{.Description}}{{end}}{{end}}{{end}}{{if .Subcommands}}
+
+  Subcommands:
+{{range .Subcommands}}
+    {{.LongName}}{{if .ShortName}} ({{.ShortName}}){{end}}{{if .Position}} (Position {{.Position}}){{end}}{{if .Description}} {{.Description}}{{end}}{{end}}{{end}}{{if (gt (len .StringFlags) 0) | (gt (len .IntFlags) 0) | (gt (len .BoolFlags) 0)}}
+
+  Flags:{{if (gt (len .StringFlags) 0)}}{{range .StringFlags}}
+    --{{.LongName}}{{if .ShortName}} (-{{.ShortName}}){{end}}{{if .Description}} {{.Description}}{{end}}{{end}}{{end}}{{if (gt (len .IntFlags) 0)}}{{range .IntFlags}}
+    --{{.LongName}}{{if .ShortName}} (-{{.ShortName}}){{end}}{{if .Description}} {{.Description}}{{end}}{{end}}{{end}}{{if (gt (len .BoolFlags) 0)}}{{range .BoolFlags}}
+    --{{.LongName}}{{if .ShortName}} (-{{.ShortName}}){{end}}{{if .Description}} {{.Description}}{{end}}{{end}}{{end}}{{end}}
+{{if .AppendMessage}}
+{{.AppendMessage}}{{end}}{{if .Message}}{{.Message}}
 {{end}}`
