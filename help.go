32f2b54f5fc0db8ecf17710a1334c9c5fc3b1834
Eric Greer <EricGreer@Gmail.com>
2018-04-09 10:11:02 +0800

    help template tweaking

--- help.go
+++ help.go
@@ -8,19 +8,20 @@ const defaultHelpTemplate = `{{.CommandName}}{{if .Description}} - {{.Descriptio
 
   Positional Variables:
 {{range .Positionals}}
-    {{.Name}} (Position {{.Position}}){{if .Required}} (Required){{end}} -{{if .Description}} {{.Description}}{{end}}{{end}}{{end}}{{if .Subcommands}}
+    {{.Name}} (Position {{.Position}}){{if .Required}} (Required){{end}} -{{if .Description}} {{.Description}}{{end}}{{end}}
+{{end}}{{if .Subcommands}}
 
   Subcommands:
 {{range .Subcommands}}
-    {{.LongName}}{{if .ShortName}} ({{.ShortName}}){{end}} -{{if .Position}}{{if gt .Position 1}} (Position {{.Position}}){{end}}{{end}}{{if .Description}} {{.Description}}{{end}}{{end}}{{end}}{{if (or (or (gt (len .StringFlags) 0) (gt (len .IntFlags) 0)) (gt (len .BoolFlags) 0))}}
-
+    {{.LongName}}{{if .ShortName}} ({{.ShortName}}){{end}} -{{if .Position}}{{if gt .Position 1}} (Position {{.Position}}){{end}}{{end}}{{if .Description}} {{.Description}}{{end}}{{end}}
+{{end}}{{if (or (or (gt (len .StringFlags) 0) (gt (len .IntFlags) 0)) (gt (len .BoolFlags) 0))}}
   Flags:
   {{if .StringFlags}}{{range .StringFlags}}
     {{if .LongName}}--{{.LongName}} {{end}}{{if .ShortName}}(-{{.ShortName}}){{end}}{{if .Description}} {{.Description}}{{end}}{{end}}{{end}}{{if .IntFlags}}{{range .IntFlags}}
     {{if .LongName}}--{{.LongName}} {{end}}{{if .ShortName}}(-{{.ShortName}}){{end}}{{if .Description}} {{.Description}}{{end}}{{end}}{{end}}{{if .BoolFlags}}{{range .BoolFlags}}
     {{if .LongName}}--{{.LongName}} {{end}}{{if .ShortName}}(-{{.ShortName}}){{end}}{{if .Description}} {{.Description}}{{end}}{{end}}{{end}}
 {{end}}
-{{if .AppendMessage}}
-{{.AppendMessage}}{{end}}{{if .Message}}
+{{if .AppendMessage}}{{.AppendMessage}}
+{{end}}{{if .Message}}
 {{.Message}}
 {{end}}`
