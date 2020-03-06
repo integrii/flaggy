4259f0d8512b45a5cf748e15992bf33912beed8f
Pavel Patrin <pavelpatrin@gmail.com>
2020-03-06 18:26:29 +0300

    Changed module interface and help message.
    * Added shortcut Positional() for morely balanced API.
    * Replaced "Positional arguments" with "Positionals".
    * Added newline after positional arguments.

--- help.go
+++ help.go
@@ -9,12 +9,13 @@ const defaultHelpTemplate = `{{.CommandName}}{{if .Description}} - {{.Descriptio
   Usage:
     {{.UsageString}}{{end}}{{if .Positionals}}
 
-  Positional Variables: {{range .Positionals}}
+  Positionals: {{range .Positionals}}
     {{.Name}}  {{.Spacer}}{{if .Description}} {{.Description}}{{end}}{{if .DefaultValue}} (default: {{.DefaultValue}}){{else}}{{if .Required}} (Required){{end}}{{end}}{{end}}{{end}}{{if .Subcommands}}
 
   Subcommands: {{range .Subcommands}}
     {{.LongName}}{{if .ShortName}} ({{.ShortName}}){{end}}{{if .Position}}{{if gt .Position 1}}  (position {{.Position}}){{end}}{{end}}{{if .Description}}   {{.Spacer}}{{.Description}}{{end}}{{end}}
 {{end}}{{if (gt (len .Flags) 0)}}
+
   Flags: {{if .Flags}}{{range .Flags}}
     {{if .ShortName}}-{{.ShortName}} {{else}}   {{end}}{{if .LongName}}--{{.LongName}}{{end}}{{if .Description}}   {{.Spacer}}{{.Description}}{{if .DefaultValue}} (default: {{.DefaultValue}}){{end}}{{end}}{{end}}{{end}}
 {{end}}{{if .AppendMessage}}{{.AppendMessage}}
